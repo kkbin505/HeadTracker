07aee8de15881f5b286c91682fc75ec90fe60bc2
kkbin505 <19462942@qq.com>
2022-10-07 19:58:33 +0800

    Modify sbus to crsf

--- firmware/src/src/SBUS/sbus.h
+++ firmware/src/src/SBUS/sbus.h
@@ -27,3 +27,23 @@ void sbus_init();
 void sbus_Thread();
 bool SBUS_Read_Data(uint16_t ch_[16]);
 void SBUSIn_SetInverted(bool sbusininv);
+
+// ELRS command
+#define ELRS_ADDRESS                    0xEE
+#define ELRS_BIND_COMMAND               0xFF
+#define ELRS_WIFI_COMMAND               0xFE
+#define ELRS_PKT_RATE_COMMAND           0x01
+#define ELRS_TLM_RATIO_COMMAND          0x02
+#define ELRS_SWITCH_MODE_COMMAND        0x03
+#define ELRS_MODEL_MATCH_COMMAND        0x04
+#define ELRS_POWER_COMMAND              0x06
+#define ELRS_BLE_JOYSTIC_COMMAND        17
+#define TYPE_SETTINGS_WRITE             0x2D
+#define ADDR_RADIO                      0xEA //  Radio Transmitter
+#define ELRS_FRAME_LENGTH               24 // length of type + payload + crc
+#define TYPE_CHANNELS                   0x16
+
+// Define AUX channel input limite
+#define CRSF_DIGITAL_CHANNEL_MIN 172
+#define CRSF_DIGITAL_CHANNEL_MID 992
+#define CRSF_DIGITAL_CHANNEL_MAX 1811
